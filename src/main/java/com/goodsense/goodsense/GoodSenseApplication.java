9cc11236e5a9dc231c61389e337984f4c99da20a
Rachel Skwerer <rachelskwerer@gmail.com>
2021-05-04 22:00:54 -0400

    first commit

---
+++ src/main/java/com/goodsense/goodsense/GoodSenseApplication.java
@@ -0,0 +1,13 @@
+package com.goodsense.goodsense;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoodSenseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoodSenseApplication.class, args);
+	}
+
+}
